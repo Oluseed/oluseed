95d0d3b51a62bf6f5660a8f1834a26f4ff644c29
Oduoye Ayomide <ayomidecoder@gmail.com>
2025-07-07 23:54:10 +0100

    Completed the services section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import { Hero_Section } from '../components/home/Hero_Section'
-import { User_Section_1 } from '../components/home/User_Section_1'
+import { OurService } from '../components/home/OurService'
 
 export const Home: React.FC = () => {
   return (
     <div className="relative">
-      {/* Hero background section */}
       <Hero_Section />
-
-      {/* User Section overlayed on Hero Section in md and up */}
-      <div className="md:absolute md:top-[75%] md:left-1/2 md:-translate-x-1/2 w-full px-4 md:px-0 z-20 lg:w-[auto]">
-        <User_Section_1 />
-      </div>
+      <OurService/>
     </div>
   )
 }
